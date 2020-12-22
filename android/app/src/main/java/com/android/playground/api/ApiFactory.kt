abea60d562700e004fd07bce398d71e9cd2c1969
Simon Chae <schae@creditsesame.com>
2020-12-22 11:06:02 -0500

    Add Api support for user and transaction

---
+++ android/app/src/main/java/com/android/playground/api/ApiFactory.kt
@@ -0,0 +1,30 @@
+package com.android.playground.api
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+/**
+ * Factory class for creating the Retrofit instance.
+ */
+class ApiFactory(private val host: String = HOST) {
+    fun create(): Api {
+        val httpClient = OkHttpClient.Builder()
+            .connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(host)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient)
+            .build()
+            .create(Api::class.java)
+    }
+
+    companion object {
+        private const val TIMEOUT_SECONDS = 24L
+        private const val HOST = "https://localhost:3000"
+    }
+}
