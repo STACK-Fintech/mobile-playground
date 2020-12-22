4490b8ce084c67c74cf226981fc8e239ecfa91cf
Simon Chae <schae@creditsesame.com>
2020-12-22 11:55:28 -0500

    Add Rx support

--- android/app/src/main/java/com/android/playground/api/Api.kt
+++ android/app/src/main/java/com/android/playground/api/Api.kt
@@ -2,13 +2,28 @@ package com.android.playground.api
 
 import com.android.playground.models.Transaction
 import com.android.playground.models.User
+import io.reactivex.rxjava3.core.Observable
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface Api {
+
     @GET("users")
     suspend fun getUsers(): Response<List<User>>
 
+    @GET("users")
+    fun getUsersRx(): Observable<List<User>>
+
+    @GET("users")
+    fun getUsersCall(): Call<Response<List<User>>>
+
     @GET("transactions")
     suspend fun getTransactions(): Response<List<Transaction>>
+
+    @GET("users")
+    fun getTransactionsRx(): Observable<List<Transaction>>
+
+    @GET("users")
+    fun getTransactionsCall(): Call<Response<List<Transaction>>>
 }
