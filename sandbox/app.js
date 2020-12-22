bbaee8f62ca17ff3084c40949e4a855d198454a0
Simon Chae <schae@creditsesame.com>
2020-12-22 16:00:11 -0500

    Add mock users and transactions data

--- sandbox/app.js
+++ sandbox/app.js
@@ -2,15 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dbHandler = require('./api/helpers/db-handler');
-const app = express();
-
+const mockReader = require('./api/helpers/mock-reader')
 const usersRoute = require("./api/routes/users")
 const transRoute = require("./api/routes/transactions")
+const app = express();
 
 app.use(bodyParser.json());
 app.use("/users", usersRoute);
 app.use("/transactions", transRoute);
 
 dbHandler.connect().then( async() => {
+    mockReader.loadUsers()
+    mockReader.loadTransactions()
     app.listen(3000);
 });
