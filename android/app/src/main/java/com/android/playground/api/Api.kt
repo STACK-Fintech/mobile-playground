ff28d70a8d0607d90a69345f8201578c3b1fc971
Simon Chae <schae@creditsesame.com>
2020-12-22 12:15:57 -0500

    Update the API to Call and add Rx adapter

--- android/app/src/main/java/com/android/playground/api/Api.kt
+++ android/app/src/main/java/com/android/playground/api/Api.kt
@@ -2,7 +2,7 @@ package com.android.playground.api
 
 import com.android.playground.models.Transaction
 import com.android.playground.models.User
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
@@ -13,17 +13,17 @@ interface Api {
     suspend fun getUsers(): Response<List<User>>
 
     @GET("users")
-    fun getUsersRx(): Observable<List<User>>
+    fun getUsersRx(): Single<List<User>>
 
     @GET("users")
-    fun getUsersCall(): Call<Response<List<User>>>
+    fun getUsersCall(): Call<List<User>>
 
     @GET("transactions")
     suspend fun getTransactions(): Response<List<Transaction>>
 
-    @GET("users")
-    fun getTransactionsRx(): Observable<List<Transaction>>
+    @GET("transactions")
+    fun getTransactionsRx(): Single<List<Transaction>>
 
-    @GET("users")
-    fun getTransactionsCall(): Call<Response<List<Transaction>>>
+    @GET("transactions")
+    fun getTransactionsCall(): Call<List<Transaction>>
 }
