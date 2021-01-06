678038b5b2e8de3b5028acf7d2f5575e9a1be9c0
Simon Chae <schae@creditsesame.com>
2021-01-06 10:23:59 -0500

    Add weak referencing to bases presenter for low memory

--- android/app/src/main/java/com/android/playground/view/presenters/BasePresenter.kt
+++ android/app/src/main/java/com/android/playground/view/presenters/BasePresenter.kt
@@ -4,27 +4,30 @@ import androidx.annotation.MainThread
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
+import java.lang.ref.WeakReference
 
 abstract class BasePresenter<ViewController> : LifecycleObserver {
-    private var viewController: ViewController? = null
-    private var viewLifecycle: Lifecycle? = null
+    private var viewController = WeakReference<ViewController>(null)
+    private var viewLifecycle = WeakReference<Lifecycle>(null)
 
     @MainThread
     open fun attachView(viewController: ViewController, viewLifecycle: Lifecycle) {
-        this.viewController = viewController
-        this.viewLifecycle = viewLifecycle.apply {
-            addObserver(this@BasePresenter)
-        }
+        this.viewController = WeakReference(viewController)
+        this.viewLifecycle = WeakReference(
+            viewLifecycle.apply {
+                addObserver(this@BasePresenter)
+            }
+        )
     }
 
     protected open fun onClear() {}
 
-    protected fun viewController(): ViewController? = viewController
+    protected fun viewController(): ViewController? = viewController.get()
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     private fun onViewDestroyed() {
-        viewController = null
-        viewLifecycle = null
+        viewController.clear()
+        viewLifecycle.clear()
         onClear()
     }
 }
