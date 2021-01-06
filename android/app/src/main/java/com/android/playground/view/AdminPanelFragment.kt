4f7d5dc60d2ad04b7d2516a264235ff67e785d53
Simon Chae <schae@creditsesame.com>
2021-01-06 10:08:07 -0500

    Add viewModel and presenter boilerplates for fragment

--- android/app/src/main/java/com/android/playground/view/AdminPanelFragment.kt
+++ android/app/src/main/java/com/android/playground/view/AdminPanelFragment.kt
@@ -5,9 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.android.playground.R
+import com.android.playground.view.presenters.AdminPanelPresenter
+import com.android.playground.view.presenters.AdminPanelViewController
+import com.android.playground.view.viewmodels.AdminPanelViewModel
+import com.android.playground.view.viewmodels.ViewModelFactory
 
-class AdminPanelFragment : Fragment() {
+class AdminPanelFragment : Fragment(), AdminPanelViewController {
+    private val viewModel: AdminPanelViewModel by lazy {
+        ViewModelProvider(
+            this,
+            ViewModelFactory()
+        )[AdminPanelViewModel::class.java]
+    }
+
+    private val presenter: AdminPanelPresenter by lazy { AdminPanelPresenter() }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         inflater.inflate(R.layout.fragment_admin_panel, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        presenter.attachView(this, lifecycle)
+        bindUI()
+    }
+
+    private fun bindUI() {
+        // TODO
+    }
 }
