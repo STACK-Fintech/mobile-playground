3e6cc0b06dcfc5a3b2ba78d3613a91e6d50c9880
Wenzhong Zheng <andrew198712@gmail.com>
2021-01-16 21:41:44 -0500

    add tableView and loader

---
+++ ios/app/app/MainBaseViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MainBaseViewController.swift
+//  app
+//
+//  Created by Wenzhong on 2021-01-16.
+//
+
+import UIKit
+
+class MainBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+        view.addSubview(activityIndicatorView)
+        activityIndicatorView.center = view.center
+    }
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    private lazy var activityIndicatorView: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView(style: .large)
+        indicator.color = .blue
+        indicator.hidesWhenStopped = true
+        indicator.startAnimating()
+        return indicator
+    }()
+}
